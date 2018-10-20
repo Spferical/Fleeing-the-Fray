148e66bc4ceb6433c77fc985d9f9cafe9c4e4085
Matthew Pfeiffer <spferical@gmail.com>
2018-10-20 04:58:04 -0400

    Bigger font
    I'm not gonna add a font picker to this old project, but darn did I pick a wonky resolution, and darn does 8x8 unscaled SDL not age well.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from config import SCREEN_WIDTH, SCREEN_HEIGHT, MAX_FPS
 
 
 def main():
+    tcod.console_set_custom_font(
+        'terminal16x16.png',
+        tcod.FONT_LAYOUT_ASCII_INCOL,
+    )
     tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'Fleeing the Fray',
                            False)
     tcod.sys_set_fps(MAX_FPS)
